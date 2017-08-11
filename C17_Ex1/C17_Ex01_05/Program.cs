8f45323a44b094da057566389108dcbdac722655
Jacob Roginsky <jacobroginsky@gmail.com>
2017-08-11 23:00:03 +0300

    add documentation to Ex01_05

--- C17_Ex1/C17_Ex01_05/Program.cs
+++ C17_Ex1/C17_Ex01_05/Program.cs
@@ -6,45 +6,54 @@ namespace C17_Ex01_05
     class Program
     {
         const uint k_AmountOfDigitsInNumber = 10;
+        const char k_zeroDigit = '0';
+        const char k_nineDigit = '9';
 
         static void Main(string[] args)
         {
             GetNumberAndCalculateStatistics();
         }
 
+        // Gets a number from user and calculates it's statistics
         public static void GetNumberAndCalculateStatistics()
         {
             string inputNumberStr = getInputNumberFromUser();
             calculateAndPrintNumericStatistics(inputNumberStr);
         }
 
+        // Calculates numeric statistics for for an input number and prints the statistics
         private static void calculateAndPrintNumericStatistics(string inputNumberStr)
         {
             printNumericStatistics(сalculateNumericStatistics(inputNumberStr));
         }
 
+        // Prints numeric statistics
         private static void printNumericStatistics(NumericStatistics i_NumericStatistics)
         {
             System.Console.WriteLine("==================={0}Numeric Statistics:{0}===================", System.Environment.NewLine);
             System.Console.WriteLine(
                 "The biggest digit is {1}{0}The smallest digit is {2}{0}The amount of digits bigger than units digit is {3}{0}The amount of digits smaller than units digit is {4}",
                 System.Environment.NewLine,
-                i_NumericStatistics.BiggestDigit,
-                i_NumericStatistics.SmallestDigit,
-                i_NumericStatistics.AmountOfDigitsBiggerThanUnitsDigit,
-                i_NumericStatistics.AmountOfDigitsSmallerThanUnitsDigit);
+                i_NumericStatistics.m_BiggestDigit,
+                i_NumericStatistics.m_SmallestDigit,
+                i_NumericStatistics.m_AmountOfDigitsBiggerThanUnitsDigit,
+                i_NumericStatistics.m_AmountOfDigitsSmallerThanUnitsDigit);
         }
 
+        // Calculates numeric statistics
         private static NumericStatistics сalculateNumericStatistics(string inputNumberStr)
         {
             NumericStatistics o_NumericStatistics;
-            o_NumericStatistics.BiggestDigit = getBiggestDigitInNumericString(inputNumberStr);
-            o_NumericStatistics.SmallestDigit = getSmallestDigitInNumericString(inputNumberStr);
-            o_NumericStatistics.AmountOfDigitsBiggerThanUnitsDigit = getAmountOfDigitsBiggerThanUnitsDigit(inputNumberStr);
-            o_NumericStatistics.AmountOfDigitsSmallerThanUnitsDigit = getAmountOfDigitsSmallerThanUnitsDigit(inputNumberStr);
+
+            o_NumericStatistics.m_BiggestDigit = getBiggestDigitInNumericString(inputNumberStr);
+            o_NumericStatistics.m_SmallestDigit = getSmallestDigitInNumericString(inputNumberStr);
+            o_NumericStatistics.m_AmountOfDigitsBiggerThanUnitsDigit = getAmountOfDigitsBiggerThanUnitsDigit(inputNumberStr);
+            o_NumericStatistics.m_AmountOfDigitsSmallerThanUnitsDigit = getAmountOfDigitsSmallerThanUnitsDigit(inputNumberStr);
+
             return o_NumericStatistics;
         }
 
+        // Counts the amount of digits that are smaller than the units digit
         private static uint getAmountOfDigitsSmallerThanUnitsDigit(string inputNumberStr)
         {
             uint amountOfDigitsSmallerThanUnitsDigitCounter = 0;
@@ -61,6 +70,7 @@ namespace C17_Ex01_05
             return amountOfDigitsSmallerThanUnitsDigitCounter;
         }
 
+        // Counts the amount of digits that are bigger than the units digit
         private static uint getAmountOfDigitsBiggerThanUnitsDigit(string inputNumberStr)
         {
             uint amountOfDigitsBiggerThanUnitsDigitCounter = 0;
@@ -77,9 +87,10 @@ namespace C17_Ex01_05
             return amountOfDigitsBiggerThanUnitsDigitCounter;
         }
 
+        // Gets the biggest digit in a numeric string
         private static uint getBiggestDigitInNumericString(string i_NumericStr)
         {
-            char maxDigit = '0';
+            char maxDigit = k_zeroDigit;
 
             foreach (char digit in i_NumericStr)
             {
@@ -92,9 +103,10 @@ namespace C17_Ex01_05
             return uint.Parse(maxDigit.ToString());
         }
 
+        // Gets the smallest digit in a numeric string
         private static uint getSmallestDigitInNumericString(string i_NumericStr)
         {
-            char minDigit = '9';
+            char minDigit = k_nineDigit;
 
             foreach (char digit in i_NumericStr)
             {
@@ -107,6 +119,7 @@ namespace C17_Ex01_05
             return uint.Parse(minDigit.ToString());
         }
 
+        // Gets a numeric string from user
         private static string getInputNumberFromUser()
         {
             System.Console.WriteLine("Please enter a number with the length of {0} digits:", k_AmountOfDigitsInNumber);
@@ -121,12 +134,13 @@ namespace C17_Ex01_05
             return userInputStr;
         }
         
+        // a struct to hold the numeric statistics
         struct NumericStatistics
         {
-            public uint BiggestDigit;
-            public uint SmallestDigit;
-            public uint AmountOfDigitsBiggerThanUnitsDigit; 
-            public uint AmountOfDigitsSmallerThanUnitsDigit; 
+            public uint m_BiggestDigit;
+            public uint m_SmallestDigit;
+            public uint m_AmountOfDigitsBiggerThanUnitsDigit; 
+            public uint m_AmountOfDigitsSmallerThanUnitsDigit; 
         }
     }
 }
