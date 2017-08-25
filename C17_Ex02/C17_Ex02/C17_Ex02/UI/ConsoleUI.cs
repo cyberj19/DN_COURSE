99177aac4797845b865f02b7f34848947d129255
Jacob Roginsky <jacob.roginsky@sizmek.com>
2017-08-25 18:50:19 +0300

    DrawBoard now works

--- C17_Ex02/C17_Ex02/C17_Ex02/UI/ConsoleUI.cs
+++ C17_Ex02/C17_Ex02/C17_Ex02/UI/ConsoleUI.cs
@@ -1,47 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using C17_Ex02.BasicDataTypes;
 using C17_Ex02.Game;
 using C17_Ex02.Utils;
 using C17_Ex02.Generators;
 
-
 namespace C17_Ex02.UI
 {
     class ConsoleUI
     {
+        const byte k_NumberOfSignsPerColumn = 4;
+
         public static void DrawBoard(Board<GameBoardCell> i_Board)
         {
             byte counter = 1;
 
-            RangeGenerator range = new RangeGenerator(2, (int)i_Board.Cols+1);
-            while (!range.HasFinished()){
-                Console.Write(range.Next().ToString() + ConsoleUtils.k_SpaceChar + ConsoleUtils.k_SpaceChar);
-            }
-            
-
-
-            //run over the board
-            TwoDimensionalArrayGenerator<GameBoardCell> generator = i_Board.GetItemsGenerator();
-
-            while (!generator.HasFinished())
+            Console.Write(ConsoleUtils.k_SpaceChar.ToString());
+            RangeGenerator range = new RangeGenerator(1, (int)i_Board.Cols + 1);
+            while (!range.HasFinished())
             {
-                if (generator.IsOuterIterChangedSinceLastCheck())
-                {
-                    Console.WriteLine();
-                    //this is a new line...
-                    Console.Write(counter.ToString() + ConsoleUtils.k_SpaceChar + ConsoleUtils.k_ColumnsDelimiter + ConsoleUtils.k_SpaceChar);
-                    counter++;
-                }
+                Console.Write(ConsoleUtils.k_SpaceChar + range.Next().ToString() + ConsoleUtils.k_SpaceChar + ConsoleUtils.k_SpaceChar);
+            }
 
+            Console.WriteLine();
+
+            // Run over the board
+            TwoDimensionalArrayGenerator<GameBoardCell> generator = i_Board.GetItemsGenerator();
+            generator.IsOuterIterChangedSinceLastCheck();
+            ConsoleUtils.PrintNumericMargins("{0}" + ConsoleUtils.k_ColumnsDelimiter, counter++);
+            while (!generator.HasFinished())
+            {              
                 GameBoardCell currCell = generator.Next();
-                //do something with current cell..
-                Console.Write(currCell.ToString() + ConsoleUtils.k_ColumnsDelimiter);
+                Console.Write(ConsoleUtils.k_SpaceChar + currCell.ToString() + ConsoleUtils.k_SpaceChar + ConsoleUtils.k_ColumnsDelimiter); 
+
+                if (generator.IsOuterIterChangedSinceLastCheck())   
+                {
+                    // print border
+                    ConsoleUtils.PrintBorder(ConsoleUtils.k_BorderUnitSign, i_Board.Cols * k_NumberOfSignsPerColumn, ConsoleUtils.k_SpaceChar.ToString() + ConsoleUtils.k_BorderUnitSign.ToString());
+
+                    if (!generator.HasFinished())
+                    {
+                        ConsoleUtils.PrintNumericMargins("{0}" + ConsoleUtils.k_ColumnsDelimiter, counter++);
+                    }
+                }     
             }
         }
-
     }
 }
--- C17_Ex02/C17_Ex02/C17_Ex02/Utils/ConsoleUtils.cs
+++ C17_Ex02/C17_Ex02/C17_Ex02/Utils/ConsoleUtils.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using C17_Ex02.Generators;
 
 namespace C17_Ex02.Utils
 {
-    class ConsoleUtils
+    public class ConsoleUtils
     {
         public const char k_ColumnsDelimiter = '|';
         public const char k_SpaceChar = ' ';
+        public const char k_BorderUnitSign = '=';
+
+
+        public static void PrintBorder(char i_BorderUnitSign, uint i_NumberOfUnits, string i_MarginStr)
+        {
+            Console.WriteLine();
+            PrintMargins(i_MarginStr);
+            RangeGenerator range = new RangeGenerator((int)i_NumberOfUnits);
+            while (!range.HasFinished())
+            {
+                Console.Write(i_BorderUnitSign);
+                range.Next();
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintNumericMargins(string i_MarginStr, int i_RowNumber)
+        {
+            PrintMargins(String.Format(i_MarginStr, i_RowNumber));
+        }
+
+        public static void PrintMargins(string i_MarginStr)
+        {
+            Console.Write(i_MarginStr);
+        }
 
     }
 }
