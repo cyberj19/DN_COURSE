1b1fbcb3cf1615b17adc36e3fedbd80a9d02bace
Jacob Roginsky <jacob.roginsky@sizmek.com>
2017-08-25 14:18:13 +0300

    added ConsoleUI and ConsoleUtils

---
+++ C17_Ex02/C17_Ex02/C17_Ex02/UI/ConsoleUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C17_Ex02.UI
+{
+    class ConsoleUI
+    {
+    }
+}
